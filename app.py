85b4210debd86c80cf33777bdab6594cdaee1877
hi_math <92261755+hi-math@users.noreply.github.com>
2023-12-02 08:58:19 +0900

    Update app.py

--- app.py
+++ app.py
@@ -3,6 +3,7 @@ import pandas as pd
 from streamlit_gsheets import GSheetsConnection
 import time
 from streamlit_extras.switch_page_button import switch_page 
+from gspread.exceptions import APIError
 
 st.image("images/bg.png")
 st.header('	:seedling:서울특별시 :blue[환경 데이터] 플랫폼', divider="rainbow")
@@ -10,7 +11,7 @@ st.header('	:seedling:서울특별시 :blue[환경 데이터] 플랫폼', divide
 st.divider()
 
 if 'name' not in st.session_state:
-    st.session_state['name'] = ''
+    st.session_state['name'] = 'noname'
     
     
 if 'pw' not in st.session_state:
@@ -25,42 +26,68 @@ with st.form("로그인"):
         st.session_state['pw'] = st.text_input("비밀번호", key="b", type="password")
     submitted = st.form_submit_button("로그인")
 
+login = {"박시현":1113,
+         "성지은":1408,
+         "유지우":1412,
+         "이유진":1414,
+         "조수아":1416,
+         "조현서":2111,
+         "김서진":2203,
+         "김나경":2304,
+         "이정민":2315,
+         "유준서":2410,
+         "정희선":2413,
+         "김진영":3204,
+         "이서연":3210,
+         "안영석":1000}
+
+
 if submitted:
     with st.spinner('로그인중입니다...'):
         time.sleep(2)
-    if len(st.session_state['pw'])>7:
-        st.caption(f"{st.session_state['name']}님 로그인 되었습니다.")
-   
     
-        conn = st.connection("gsheets", type=GSheetsConnection)
-
-        df = conn.read(
+    if st.session_state['name'] in login.keys():
+        
+        if st.session_state['pw'] == str(login[st.session_state['name']]):
+        
+            st.caption(f"{st.session_state['name']}님 로그인 되었습니다.")
+            conn = st.connection("gsheets", type=GSheetsConnection)
+            df = conn.read(
             worksheet="Sheet1",
             ttl="10m",
             usecols=[0, 1],
             nrows=100
         )
-        df = df.dropna(axis=0)
-
-        df_new = pd.DataFrame([{"name": st.session_state['name'],
-                            "pw": st.session_state['pw']
-                            }])
-        df_copy = df.copy()
-        df_result = pd.concat([df_copy,df_new], ignore_index=True)
-        conn.update(worksheet="Sheet1", data=df_result)
-
-
-        my_bar = st.progress(0, text='잠시후 페이지를 이동합니다.')
-
-        for percent_complete in range(100):
-            time.sleep(0.01)
-            my_bar.progress(percent_complete + 1, text='잠시후 페이지를 이동합니다.')
+            df = df.dropna(axis=0)
+            df1 = pd.DataFrame({"name":st.session_state['name'],
+                    "loc":"",
+                    "reason":""}, index=[0])
         
-        switch_page("데이터 살펴보기")
+        
+            try:
+              conn.create(worksheet=st.session_state['name'], data=df1)
+            except APIError as e:
+              if e.response.status_code == 400 and 'already exists' in str(e):
+                pass
+              else:
+                raise  # 다른 APIError의 경우, 예외를 다시 발생시킴
+                        
 
+            my_bar = st.progress(0, text='잠시후 페이지를 이동합니다.')
+
+            for percent_complete in range(100):
+              time.sleep(0.01)
+              my_bar.progress(percent_complete + 1, text='잠시후 페이지를 이동합니다.')
+        
+            switch_page("데이터 살펴보기")
+        
+        else:
+          st.write("로그인 정보가 잘못되었습니다.")
+        
     else:
-        st.caption("8자 보다 긴 비밀번호를 설정하세요.")
-
+          st.write("로그인 정보가 잘못되었습니다.")
+          
+              
 
 # if st.button("new sheet"):
 #     df1 = pd.DataFrame({"A":[1,2,3],

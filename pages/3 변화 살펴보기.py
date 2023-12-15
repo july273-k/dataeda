460d2b616f068d74937cde035ddcefea1010e308
hi_math <92261755+hi-math@users.noreply.github.com>
2023-12-16 07:32:17 +0900

    Update 3 변화 살펴보기.py

--- pages/3 변화 살펴보기.py
+++ pages/3 변화 살펴보기.py
@@ -3,14 +3,18 @@ from streamlit_folium import st_folium, folium_static
 import pandas as pd
 import random
 
-st.sidebar.caption(f'{st.session_state['loc']} 환경정책과 {st.session_state['name']}
 
 st.image("images/bg.png")
 st.header('	:seedling:변화 살펴보기', divider="rainbow")
 
 if 'data' not in st.session_state:
     st.session_state['data'] = pd.read_csv("datas/life_trash.csv")
-    
+if 'loc' not in st.session_state:
+    st.session_state['loc'] = "관악구"
+
+st.sidebar.caption(f'{st.session_state['loc']} 환경정책과 {st.session_state['name']}
+
+
 df = st.session_state['data']
 
 with st.form("정보입력"):

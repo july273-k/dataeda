0050bf5fb5856821ef3d52561988610539cb835a
hi_math <92261755+hi-math@users.noreply.github.com>
2023-12-16 07:55:34 +0900

    Update 1 데이터 살펴보기.py

--- pages/1 데이터 살펴보기.py
+++ pages/1 데이터 살펴보기.py
@@ -25,7 +25,7 @@ st.session_state['data'] = st.data_editor(df, use_container_width=True)
 
 st.session_state['loc'] = random.choice(["종로구","중구","용산구","성동구","광진구","동대문구","중랑구","성북구","강북구","도봉구","노원구","은평구","서대문구","마포구","양천구","강서구","구로구","금천구","영등포구","동작구","관악구","서초구","강남구","송파구","강동구"])
 
-st.sidebar.caption(f'{st.session_state['loc']} 환경정책과 {st.session_state['name']})
+st.sidebar.caption(f"{st.session_state['loc']} 환경정책과 {st.session_state['name']}")
 
 st.write("데이터 사이에는 어떤 관계가 있나요:question::question:")
 

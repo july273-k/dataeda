f39f77662e3d59aa597f22d565904c6fd5be33c4
hi_math <92261755+hi-math@users.noreply.github.com>
2023-12-16 07:28:30 +0900

    Update 1 데이터 살펴보기.py

--- pages/1 데이터 살펴보기.py
+++ pages/1 데이터 살펴보기.py
@@ -2,6 +2,7 @@ import streamlit as st
 import pandas as pd
 from streamlit_folium import st_folium, folium_static
 import time
+import random
 
 st.image("images/bg.png")
 st.header('	:seedling:데이터 살펴보기', divider="rainbow")
@@ -12,7 +13,19 @@ df = pd.read_csv("./datas/life_trash.csv")
 if 'data' not in st.session_state:
     st.session_state['data'] = pd.DataFrame()
 
+if 'loc' not in st.session_state:
+    st.session_state['loc'] = "관악구"
+
+if 'name' not in st.session_state:
+    st.session_state['name'] = "입력없음"
+
+
+
 st.session_state['data'] = st.data_editor(df, use_container_width=True)
+
+st.session_state['loc'] = random.choice(["종로구","중구","용산구","성동구","광진구","동대문구","중랑구","성북구","강북구","도봉구","노원구","은평구","서대문구","마포구","양천구","강서구","구로구","금천구","영등포구","동작구","관악구","서초구","강남구","송파구","강동구"])
+
+st.sidebar.caption(f'{st.session_state['loc']} 환경정책과 {st.session_state['name']}
 
 st.write("데이터 사이에는 어떤 관계가 있나요:question::question:")
 
